3b07cf42e14d0f65f15bade64dae839588e2c182
laxmi13579 <laxmi.yadav370515@gmail.com>
2025-05-06 23:46:36 +0530

    dict iterate using for loop

---
+++ loops/dict_iterate_using_for.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Alice", "age": 20, "major": "Computer Science"},
+    {"name": "Bob", "age": 22, "major": "Mathematics"},
+    {"name": "Charlie", "age": 21, "major": "Physics"}
+]
+
+for student in students:
+    print(f"Name: {student["name"]} age: {student["age"]} major: {student["major"]}")
