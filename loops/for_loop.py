a969d7a0bd020a5f459a682a578f14a807fbc03e
laxmi13579 <laxmi.yadav370515@gmail.com>
2025-05-06 23:27:31 +0530

    for program

---
+++ loops/for_loop.py
@@ -0,0 +1,13 @@
+# for variable in iterable:
+
+for i in range(5):
+    print(i)
+
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# i does not require so use _ is the best way.
+for _ in range(10):
+    print('Meow')
